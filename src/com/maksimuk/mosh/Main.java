e25f79aac55f3571b4c5fa936bb4b3a423cb5a90
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-06-13 23:21:50 -0700

    General Java Introduction

---
+++ src/com/maksimuk/mosh/Main.java
@@ -0,0 +1,28 @@
+package com.maksimuk.mosh;
+
+                                                            /*General*/
+//Java was developed by James Gosling in 1995 at Sun Microsystems and later was acquired by Oracle.
+
+// class is a container for related functions. We use it to organize our code
+//Packages are containers where we group related classes
+//Every program has Main class that contains the Main Function (Method)
+// Methods are smallest building blocks in Java programs
+//Java allows us to break down programs into small building blocks that are easy to understand and use and reuse.
+//Methods have access modifiers (public - means accessible from other parts of the program. Also,private.
+//Class naming PascalNaming, methods - camelCase
+
+ public  class Main {
+   public static void main (String[] args) { //function Main is an entry point, the main function is called - code inside executes
+//function inside a class is method
+       System.out.println("Hello world!");
+    }
+}
+
+//We use JDK (Java Development Kit) to build apps. JDK has JRE (Java Runtime Environment),compiler and library of classes.
+//The structure of JRE makes Java platform independent (can be written on one OS and executed on another)
+//The Java compiler takes Java code and compiles it into Java Bytecode cross-platform format. When we run app, JVM (Java Virtual Machine) takes bytecode and translates it into OS native code.
+//
+//Java editions: Standard Edition (SE): the core; Enterprise Edition (EE): for large systems, Micro Edition (ME):for mobile devices; Java Card: used in smart cards
+
+                                                          /*Fundamentals*/
+// Variables are used for temporarily data storage. In Java, the variable should be specified at the time of declaration.
