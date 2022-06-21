60418924aeb6609cce8f4bcd923e1665a03223c4
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-06-21 16:34:00 -0700

    JavaBasics Challenge Set 1, 1/3 - completed

---
+++ src/com/maksimuk/challenge1/Main.java
@@ -0,0 +1,10 @@
+package com.maksimuk.challenge1;
+
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Tanya";
+        String lastName = "Maksimuk";
+        String fullName = firstName + " " +lastName;
+        System.out.println(fullName);
+    }
+}
