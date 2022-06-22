fd6e15e214bb0014c48ffb1a7b51917e407abc90
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-06-21 17:03:23 -0700

    challenge 3 completed

--- src/com/maksimuk/challenge1/Main.java
+++ src/com/maksimuk/challenge1/Main.java
@@ -6,5 +6,18 @@ public class Main {
         String lastName = "Maksimuk";
         String fullName = firstName + " " +lastName;
         System.out.println(fullName);
+
+        NameGenerator nameGen = new NameGenerator();
+
+        String generatedFullName = NameGenerator.generateFullName("John", "Doe");
+
+
+
+        System.out.println(createFullName("First", "Last"));
+        System.out.println(createFullName("b", "Lab"));
+    }
+
+    private static String createFullName (String firstName, String lastName){
+        return firstName +" "+ lastName;
     }
 }
