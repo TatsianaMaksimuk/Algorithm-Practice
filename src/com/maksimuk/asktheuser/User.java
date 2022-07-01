c9d0501cdf17afe96a2de42ab10ee6b03b1f3944
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-06-30 23:29:10 -0700

    askuser version 1 added

---
+++ src/com/maksimuk/asktheuser/User.java
@@ -0,0 +1,73 @@
+package com.maksimuk.asktheuser;
+
+public class User {
+
+    private byte age;
+    private short salary;
+    private int familyBudget;
+    private float creditScore;
+    private long propertyPrice;
+    private double propertyArea;
+    private boolean children;
+    private String fullName;
+
+
+    public User(byte age, short salary, int familyBudget, float creditScore, long propertyPrice, double propertyArea, boolean children, String fullName) {
+        this.age = age;
+        this.salary = salary;
+        this.familyBudget = familyBudget;
+        this.creditScore = creditScore;
+        this.propertyPrice = propertyPrice;
+        this.propertyArea = propertyArea;
+        this.children = children;
+        this.fullName = fullName;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public short getSalary() {
+        return salary;
+    }
+
+    public int getFamilyBudget() {
+        return familyBudget;
+    }
+
+    public float getCreditScore() {
+        return creditScore;
+    }
+
+    public long getPropertyPrice() {
+        return propertyPrice;
+    }
+
+    public double getPropertyArea() {
+        return propertyArea;
+    }
+
+    public boolean isChildren() {
+        return children;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "age=" + age +
+                ", salary=" + salary +
+                ", familyBudget=" + familyBudget +
+                ", creditScore=" + creditScore +
+                ", propertyPrice=" + propertyPrice +
+                ", propertyArea=" + propertyArea +
+                ", children=" + children +
+                ", fullName='" + fullName + '\'' +
+                '}';
+    }
+}
+
+
---
+++ src/com/maksimuk/asktheuser/UserInterface.java
@@ -0,0 +1,80 @@
+package com.maksimuk.asktheuser;
+
+import java.util.Scanner;
+
+public class UserInterface {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static byte age;
+    private static short salary;
+    private static int familyBudget;
+    private static float creditScore;
+    private static long propertyPrice;
+    private static long propertyArea;
+    private static boolean hasChildren;
+    private static String fullName;
+
+    public static void main(String[] args) {
+        age();
+        salary();
+        familyBudget();
+        creditScore ();
+        propertyPrice ();
+        propertyArea ();
+        children ();
+        fullName();
+
+        User generatedUser1 = new User(age, salary, familyBudget, creditScore, propertyPrice, propertyArea,hasChildren,fullName);
+        System.out.println(generatedUser1);
+    }
+
+    private static void age() {
+        System.out.println("State your age: ");
+        age = scanner.nextByte();
+        System.out.println("Age: " + age);
+    }
+
+    private static void salary() {
+        System.out.println("State your salary: ");
+        salary = scanner.nextShort();
+        System.out.println("Monthly salary: " + salary + "$");
+    }
+
+    private static void familyBudget() {
+        System.out.println("State your monthly family budget: ?");
+        familyBudget = scanner.nextInt();
+        System.out.println("Monthly family budget: " + familyBudget);
+    }
+
+    private static void creditScore () {
+        System.out.println("State your credit Score: ");
+        creditScore = scanner.nextFloat();
+        System.out.println("Credit Score: " + creditScore + "%");
+    }
+
+    private static void propertyPrice () {
+        System.out.println("State your property cost: ");
+        propertyPrice = scanner.nextLong();
+        System.out.println("Property Price: " + propertyPrice + "$");
+    }
+
+    private static void propertyArea () {
+        System.out.println("State your property area: ");
+        propertyArea = scanner.nextLong();
+        System.out.println("Property Area: " + propertyArea);
+    }
+
+    private static void children () {
+        System.out.println("Do you have children? True/False");
+        hasChildren = scanner.nextBoolean();
+        System.out.println("has children: " + hasChildren);
+    }
+
+    private static void fullName(){
+        System.out.println("What is your First Name?");
+        String firstName = scanner.nextLine();
+        System.out.println("What is your Last Name?");
+        String lastName = scanner.nextLine();
+        fullName = firstName + " " + lastName;
+        System.out.println("Name: " + fullName);
+    }
+}
