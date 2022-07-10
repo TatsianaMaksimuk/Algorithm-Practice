a108a8850e843d4d91cdb0754a4474ba49b2ac53
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-07-09 23:22:39 -0700

    My Object Factory first try

---
+++ src/com/maksimuk/objectfactory/FactoryUI.java
@@ -0,0 +1,75 @@
+package com.maksimuk.objectfactory;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class FactoryUI {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String readString(String question) {
+        while (true) {
+            System.out.println(question);
+            String input = scanner.nextLine();
+            if (!input.trim().equals("")) {
+                return input;
+            }
+        }
+    }
+
+    public static boolean readBoolean(String question) {
+        while (true) {
+            try {
+                System.out.println(question + "\n (true or false): ");
+                boolean input = scanner.nextBoolean();
+                scanner.nextLine();
+                return input;
+            } catch (Exception exception) {
+                System.out.println("Input must be <<true>> or <<false>>");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    public static short readShort(String question, short min, short max) {
+        while (true) {
+            try {
+                System.out.println(question + "\n (" + min + "-" + max + "): ");
+                short input = scanner.nextShort();
+                scanner.nextLine();
+                return input;
+            } catch (Exception exception) {
+                System.out.println("Input must be between" + min + "-" + max);
+                scanner.nextLine();
+            }
+        }
+
+    }
+
+    public static double readDouble(String question, int min, int max) {
+        while (true) {
+            try {
+                System.out.println(question + "\n(" + min + "-" + max + "): ");
+                double input = scanner.nextDouble();
+                return input;
+            } catch (Exception exception) {
+                System.out.println("Input must be between" + min + "-" + max);
+                scanner.nextLine();
+            }
+        }
+    }
+
+    public static int readInt (String question, int min, int max){
+        while (true){
+            try{
+                System.out.println(question + "\n(" + min + "-" + max + "): " );
+                int input = scanner.nextInt();
+                scanner.nextLine();
+                return input;
+            } catch(Exception exception) {
+                System.out.println("Input must be between" + min + "-" + max);
+                scanner.nextLine();
+            }
+        }
+    }
+}
+
