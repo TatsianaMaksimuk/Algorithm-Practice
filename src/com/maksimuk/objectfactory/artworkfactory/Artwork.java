daecdddaee74a1f3b2be15b7e13635537e0702a1
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-07-11 15:01:40 -0700

    My Object Factory updates:
    - added fields width and height to artwork class

--- src/com/maksimuk/objectfactory/artworkfactory/Artwork.java
+++ src/com/maksimuk/objectfactory/artworkfactory/Artwork.java
@@ -2,17 +2,23 @@ package com.maksimuk.objectfactory.artworkfactory;
 
 public class Artwork {
     private String name;
+
+
     private String author;
     private short year;
     private String medium;
+    private float widthInCm;
+    private float heightInCm;
 
 
 
-    public Artwork(String name, String author, short year, String medium) {
+    public Artwork(String name, String author, short year, String medium, float widthInCm, float heightInCm) {
         this.name = name;
         this.author = author;
         this.year = year;
         this.medium = medium;
+        this.widthInCm = widthInCm;
+        this.heightInCm = heightInCm;
     }
 
     public String getName() {
@@ -31,13 +37,24 @@ public class Artwork {
         return medium;
     }
 
+    public float getWidthInCm() {
+        return widthInCm;
+    }
+
+    public float getHeightInCm() {
+        return heightInCm;
+    }
+
+
 
     @Override
     public String toString() {
-        return "Artwork:" +"\n" +
+        return "Artwork: " + "\n" +
                 "name: " + name +
                 ", author: " + author +
                 ", year: " + year +
-                ", medium: " + medium;
+                ", medium: " + medium +
+                ", dimensions: " + widthInCm + "cm * " + heightInCm + " cm";
     }
+
 }
--- src/com/maksimuk/objectfactory/artworkfactory/MainArtwork.java
+++ src/com/maksimuk/objectfactory/artworkfactory/MainArtwork.java
@@ -7,10 +7,12 @@ public class MainArtwork {
 
         String name = FactoryUI.readString("Name: ");
         String author = FactoryUI.readString("Author: ");
-        Short year = FactoryUI.readShort("Year: ", (short) 0, (short) 2022);
+        Short year = FactoryUI.readShort("Year ", (short) 0, (short) 2022);
         String medium = FactoryUI.readString("Medium: ");
+        float widthInCm = FactoryUI.readFloat("Width ", 0, 1000000);
+        float heightInCm = FactoryUI.readFloat("Height ", 0, 1000000);
 
-        Artwork artwork01 = new Artwork(name, author,year,medium);
+        Artwork artwork01 = new Artwork(name, author,year,medium, widthInCm,heightInCm);
         System.out.println(artwork01);
     }
 }
