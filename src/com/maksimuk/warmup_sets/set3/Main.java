5fc8b0ef008e32677ea32fdce1117b847a0efef5
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-07-17 16:56:52 -0700

    Added set 3

--- src/com/maksimuk/warmup_sets/set3/Main.java
+++ src/com/maksimuk/warmup_sets/set3/Main.java
@@ -28,8 +28,20 @@ public class Main {
         System.out.println(helloName("Tiffany"));
 
         //Reverse the Order of a String
-        System.out.println(reverse("The quick brown fox.") );
+        System.out.println(reverse("The quick brown fox."));
 
+        //Area of Triangle
+        System.out.println(triArea(3, 2));
+        System.out.println(triArea(3.5, 2.5));
+
+        //Multiple of 100
+        System.out.println(divisible(1));
+        System.out.println(divisible(1000));
+        System.out.println(divisible(100));
+
+        //Capture the Rook
+        System.out.println(canCapture(new String[]{"A8", "E8"}));
+        System.out.println(canCapture(new String[]{"A1", "B2"}));
     }
 
     //Return the Sum of Two Numbers
@@ -55,18 +67,46 @@ public class Main {
     }
 
     //Name Greeting!
-    public static String helloName (String name){
+    public static String helloName(String name) {
         return "Hello, " + name;
     }
 
     //Reverse the Order of a String
 
-    public static String reverse (String str) {
+    public static String reverse(String str) {
         String result = "";
         char[] try1 = str.toCharArray();
-        for (int i = try1.length-1;i >= 0; i--) result += try1[i];
+        for (int i = try1.length - 1; i >= 0; i--) result += try1[i];
 
         return result;
+    }
+
+    //Area of Triangle
+
+    public static double triArea(double base, double height) {
+        return (base * height) / 2;
+    }
+
+    //Multiple of 100
+    public static boolean divisible(int num) {
+        if (num % 100 == 0) return true;
+        return false;
+    }
+
+    //Capture the Rook
+
+    public static boolean canCapture(String[] args) {
+        Character position1Letter = args[0].charAt(0);
+        Character position1Num = args[0].charAt(1);
+        Character position2Letter = args[1].charAt(0);
+        Character position2Num = args[1].charAt(1);
+
+        if (position1Letter.equals(position2Letter) || position1Num.equals(position2Num)) {
+            return true;
+        }
+        return false;
+
+
     }
 
 }
