fce9c358ee55edbdce9a7f82a15cbfca75211693
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-08-03 21:09:57 -0700

    "Routes added"

--- src/main/java/com/careerdevs/hellointernet/controllers/RootController.java
+++ src/main/java/com/careerdevs/hellointernet/controllers/RootController.java
@@ -2,9 +2,12 @@ package com.careerdevs.hellointernet.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 
 @RestController
 public class RootController {
+
     @GetMapping("/")
     private String routeRoute(){
         return "You requested the root route";
@@ -15,13 +18,22 @@ public class RootController {
         return "Hello, CareerDevs!";
     }
 
-    @GetMapping("/itsme")
-    private String itsme() {
-        return "it's me...";
+    @GetMapping("/me")
+    private String me() {
+        return "it's me";
     }
 
     @GetMapping("/meet")
     private String meet() {
         return "I was wondering if after all these years you'd like to meet";
+    }
+
+    //bonus
+    @GetMapping("/random")
+    private int random(){
+        Random random = new Random();
+        int upperbound = 1000;
+        int randomInt = random.nextInt(upperbound);
+        return randomInt;
     }
 }
