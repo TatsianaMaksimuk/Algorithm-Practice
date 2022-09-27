182c4147f1f11d1ec54f89d4968d7648d4b68a3b
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-09-27 13:01:48 -0700

    "Day 2 Ex"

---
+++ src/week1/MiniMaxSum.java
@@ -0,0 +1,31 @@
+package week1;
+
+import java.util.List;
+
+public class MiniMaxSum {
+    public static void findMiniNaxSum(List<Integer> arr) {
+        int lowest = arr.get(0);
+        int highest = arr.get(0);
+        int sum = 0;
+        int minSum;
+        int maxSum;
+        for (int i = 1; i < arr.size(); i++) {
+            sum += arr.get(i);
+            if (lowest > arr.get(i)) {
+                lowest = arr.get(i);
+            } else if (highest< arr.get(i)) {
+                highest = arr.get(i);
+
+            }
+        }
+        System.out.println(lowest);
+        System.out.println(highest);
+        System.out.println(sum);
+
+        minSum = sum - highest;
+        maxSum = sum - lowest;
+
+        System.out.println("Minimal sum: " + minSum);
+        System.out.println("Maximum sum: "+ maxSum);
+    }
+}
