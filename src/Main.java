427455c46025d19704489cde19df0979ef0bbf2a
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-09-27 13:05:37 -0700

    "Day 2 Ex"

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import week1.MiniMaxSum;
 import week1.PlusMinus;
+import week1.TimeConversion;
 
 import java.util.List;
 
@@ -6,8 +8,14 @@ public class Main {
     public static void main(String[] args) {
 
         //week 1
-        System.out.println("week 1:'\n'");
+        System.out.println("week 1:\n");
+        System.out.println("Ex. 1: \n");
         PlusMinus.plusMinusFunction(List.of(1, 2, -1 , -3, 0));
+
+        System.out.println("Ex.2: \n");
+        MiniMaxSum.findMiniNaxSum(List.of(1, 2, 5, 7, 9));
+
+
     }
 
 }
