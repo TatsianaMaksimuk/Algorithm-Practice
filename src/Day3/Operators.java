f40f8f7ba267ff7bdc3e9c7d9edeced9d8de242f
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-10-09 13:53:11 -0700

    Day 3 added;

---
+++ src/Day3/Operators.java
@@ -0,0 +1,13 @@
+package Day3;
+
+public class Operators {
+    public static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        // Write your code here
+        double tip_cost = (meal_cost/100) * tip_percent;
+        double tax_cost = (meal_cost/100) * tax_percent;
+        double total_cost = meal_cost+tip_cost+tax_cost;
+        // System.out.print((int)total_cost) - gives an int, we need double;
+        System.out.print(Math.round(total_cost));
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import Day0.HelloWorld;
 import Day1.DataTypes;
+import Day3.Operators;
 
 public class Main {
     public static void main(String[] args){
 
         //HelloWorld.helloMaker();
 
-        DataTypes.dataTypes();
+        //DataTypes.dataTypes();
+
+        Operators.solve(12.50, 20,5);
     }
 }
