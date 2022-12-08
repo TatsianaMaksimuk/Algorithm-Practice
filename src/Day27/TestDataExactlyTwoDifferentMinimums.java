208a425d77b06a8bdee186e76ef7a03b11720d76
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-12-07 18:33:56 -0800

    Day 27 tutorial added;

---
+++ src/Day27/TestDataExactlyTwoDifferentMinimums.java
@@ -0,0 +1,13 @@
+package Day27;
+
+public class TestDataExactlyTwoDifferentMinimums {
+    public static int[] get_array(){
+        int[] arr = new int[] {2, -6, 9, -6, 45};
+
+        return arr;
+    }
+
+    public static int get_expected_result(){
+        return 1;
+    }
+}
---
+++ src/Day27/TestDataUniqueValues.java
@@ -0,0 +1,16 @@
+package Day27;
+
+import java.util.Random;
+
+public class TestDataUniqueValues {
+
+    public static int[] get_array(){
+        int[] arr = new int[] {2, 9, -6, 45};
+
+        return arr;
+    }
+
+    public static int get_expected_result(){
+        return 2;
+    }
+}
