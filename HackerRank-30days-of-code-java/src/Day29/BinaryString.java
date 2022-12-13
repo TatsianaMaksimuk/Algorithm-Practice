20e8ffef9ee908931b60f4ccf3baf157387d4630
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-12-12 21:21:02 -0800

    Day29 tutorial added

--- HackerRank-30days-of-code-java/src/Day29/BinaryString.java
+++ HackerRank-30days-of-code-java/src/Day29/BinaryString.java
@@ -3,19 +3,36 @@ package Day29;
 public class BinaryString {
 
     //This code  converts a word and an integer to binary strings:
-    BinaryString(String string){
-        for( byte b : string.getBytes() ){
-            System.out.print(Integer.toBinaryString(b) + " ");
-        }
-        System.out.println();
-    }
+//    BinaryString(String string){
+//        for( byte b : string.getBytes() ){
+//            System.out.print(Integer.toBinaryString(b) + " ");
+//        }
+//        System.out.println();
+//    }
+//
+//    BinaryString(Integer integer){
+//        System.out.println(Integer.toBinaryString(integer));
+//    }
+//
+//    public static void main(String[] args) {
+//        new BinaryString("HackerRank");
+//        new BinaryString(8675309);
+//    }
 
-    BinaryString(Integer integer){
-        System.out.println(Integer.toBinaryString(integer));
+
+    // find and print the OR of each character in the string with 8675309:
+    BinaryString(String string, Integer integer){
+        String binaryInteger = Integer.toBinaryString(integer);
+
+        for( byte b : string.getBytes() ){
+            // Perform a bitwise operation using byte and integer operands, save result as tmp:
+            int tmp = b | integer;
+            System.out.println( Integer.toBinaryString(b) + " OR " + Integer.toBinaryString(integer)
+                    + " = " + Integer.toBinaryString(tmp) + " = " + tmp );
+        }
     }
 
     public static void main(String[] args) {
-        new BinaryString("HackerRank");
-        new BinaryString(8675309);
+        new BinaryString("HackerRank", 8675309);
     }
 }
