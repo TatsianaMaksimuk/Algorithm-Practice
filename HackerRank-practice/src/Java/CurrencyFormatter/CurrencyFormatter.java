0758ebc025d6bde5fab5b25153a59ab4bbb75da4
Tatsiana Maksimuk <okeanoka@gmail.com>
2023-02-06 21:37:09 -0800

    Currency Formatter and New Locale creation added

--- HackerRank-practice/src/Java/CurrencyFormatter/CurrencyFormatter.java
+++ HackerRank-practice/src/Java/CurrencyFormatter/CurrencyFormatter.java
@@ -1,4 +1,27 @@
 package Java.CurrencyFormatter;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class CurrencyFormatter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+        Locale India = new Locale("en", "IN");
+        String india = NumberFormat.getCurrencyInstance(India).format(payment);
+
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+
+
 }
