f837a429737d7de6ae5be3a6614da7cddb00b59f
Tatsiana Maksimuk <okeanoka@gmail.com>
2023-02-12 23:45:36 -0800

    Palindrome check added

--- HackerRank-practice/src/Java/Anagrams/AnagramSortArray.java
+++ HackerRank-practice/src/Java/Anagrams/AnagramSortArray.java
@@ -1,8 +1,9 @@
 package Java.Anagrams;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class LoopAnagram {
+public class AnagramSortArray {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String a = scan.next();
@@ -15,6 +16,11 @@ public class LoopAnagram {
         if (a.length() != b.length()) {
             return false;
         }
-        return true;
+        char[] arrayA = a.toLowerCase().toCharArray();
+        char[] arrayB = b.toLowerCase().toCharArray();
+        Arrays.sort(arrayA);
+        Arrays.sort(arrayB);
+        return Arrays.equals(arrayA,arrayB);
     }
 }
+
