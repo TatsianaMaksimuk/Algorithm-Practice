bfd1f6eddf9cc60c5ad6a126f0e4ec10a2a6bc63
Tatsiana Maksimuk <okeanoka@gmail.com>
2023-02-15 20:32:56 -0800

    Anagram check with loops added

--- HackerRank-practice/src/Java/Anagrams/AnagramsLoops.java
+++ HackerRank-practice/src/Java/Anagrams/AnagramsLoops.java
@@ -1,9 +1,11 @@
 package Java.Anagrams;
 
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class AnagramsLoops {
+    private static final int CHARACTER_RANGE= 256;
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String a = scan.next();
@@ -15,6 +17,22 @@ public class AnagramsLoops {
         }
 
     public static boolean isAnagram(String a, String b) {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        if (a.length() != b.length()) {
+            return false;
+        }
+        int count[] = new int[CHARACTER_RANGE];
+        for (int i = 0; i < a.length(); i++) {
+            count[a.charAt(i)]++;
+            count[b.charAt(i)]--;
+        }
+        for (int i = 0; i < CHARACTER_RANGE; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
         return true;
+        }
     }
-    }
+
