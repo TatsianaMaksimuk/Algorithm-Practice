30ffc44f34599c5704b977cc748c6e8d7afa6c42
Tatsiana Maksimuk <okeanoka@gmail.com>
2023-02-21 23:02:02 -0800

    added solution to find the most common number in an array

---
+++ HackerRank-practice/src/Java/BigNumber/BigNumber.java
@@ -0,0 +1,15 @@
+package Java.BigNumber;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigNumber {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        BigInteger a = s.nextBigInteger();
+        BigInteger b = s.nextBigInteger();
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+
+    }
+}
