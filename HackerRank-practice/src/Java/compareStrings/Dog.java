dd67edc11d193324a83e9e97a8984436a9bcfbd8
Tatsiana Maksimuk <okeanoka@gmail.com>
2023-02-23 18:43:37 -0800

    small input practice

---
+++ HackerRank-practice/src/Java/compareStrings/Dog.java
@@ -0,0 +1,19 @@
+package Java.compareStrings;
+
+import java.util.Scanner;
+
+public class Dog {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        if (name.equals("Zoya")){
+            System.out.println("Zoya");
+        }
+        if (name.equals("Leta")){
+            System.out.println("Leta");
+        }
+    }
+
+
+
+}
